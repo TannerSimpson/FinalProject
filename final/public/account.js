95bba4eb65ac8c3b356bfdfea45591116931c7c4
Tanner Simpson <tfs2@domain.com>
2024-04-22 22:00:42 -0400

    fixing account page

--- final/public/account.js
+++ final/public/account.js
@@ -1,69 +1,246 @@
-const form = document.getElementById("accountForm");
-const successMessage = document.getElementById("successMessage");
-const submittedInfoContainer = document.getElementById("submittedInfoContainer");
-
-form.addEventListener("submit", async function (event) {
-    event.preventDefault();
-
-    if (!validateForm()) {
-        return;
-    }
-
-    const formData = new FormData(form);
-
+const getAccounts = async () => {
     try {
-        const response = await fetch('/submit', {
-            method: 'POST',
-            body: formData
-        });
-
-        if (!response.ok) {
-            throw new Error('Submission failed');
-        }
-
-        successMessage.style.display = "block";
-        setTimeout(function () {
-            successMessage.style.display = "none";
-        }, 2000);
-
-        form.reset();
-        displaySubmittedInfo();
+      return (await fetch("api/accounts/")).json();
     } catch (error) {
-        console.error('Error:', error);
+      console.log(error);
     }
-});
+  };
+  
+  const showAccounts = async () => {
+    let accounts = await getAccounts();
+    let accountsDiv = document.getElementById("account-list");
+    accountsDiv.innerHTML = "";
+    accounts.forEach((account) => {
+      const section = document.createElement("section");
+      section.classList.add("account");
+      accountsDiv.append(section);
+  
+      const a = document.createElement("a");
+      a.href = "#";
+      section.append(a);
 
-function validateForm() {
-    return form.checkValidity();
-}
+      const img = document.createElement("img");
+      img.src = account.img;
+      a.append(img);
+  
+      const h3 = document.createElement("h3");
+      h3.innerHTML = account.name;
+      a.append(h3);
 
-async function displaySubmittedInfo() {
-    try {
-        const response = await fetch('/accounts');
-        if (!response.ok) {
-            throw new Error('Error fetching accounts');
-        }
-        const accounts = await response.json();
-        renderAccounts(accounts);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
+      const h4 = document.createElement("h4");
+      h4.innerHTML = account.nameLast;
+      a.append(h4);
 
-function renderAccounts(accounts) {
-    submittedInfoContainer.innerHTML = ''; 
-    accounts.forEach(account => {
-        const submittedInfoItem = document.createElement("div");
-        submittedInfoItem.classList.add("submitted-info-item");
-        submittedInfoItem.innerHTML = `
-            <p><strong>First Name:</strong> ${account.firstName}</p>
-            <p><strong>Last Name:</strong> ${account.lastName}</p>
-            <p><strong>Username:</strong> ${account.username}</p>
-            <p><strong>School/University:</strong> ${account.school}</p>
-            <p><strong>Grade Level:</strong> ${account.gradeLevel}</p>
-        `;
-        submittedInfoContainer.appendChild(submittedInfoItem);
+      const h5 = document.createElement("h5");
+      h5.innerHTML = account.username;
+      a.append(h5);
+
+      const h6 = document.createElement("h6");
+      h6.innerHTML = account.password;
+      a.append(h6);
+
+      const h7 = document.createElement("h7");
+      h7.innerHTML = account.school;
+      a.append(h7);
+
+      const ul = document.createElement("ul");
+      a.append(ul);
+      console.log(account.useages);
+      account.useages.forEach((useage) => {
+      const li = document.createElement("li");
+      ul.append(li);
+      li.innerHTML = useage;
+      });
+  
+      a.onclick = (e) => {
+        e.preventDefault();
+        displayDetails(account);
+      };
     });
-}
+  };
+  
+  const displayDetails = (account) => {
+    openDialog("account-details");
+    const accountDetails = document.getElementById("account-details");
+    accountDetails.innerHTML = "";
+    accountDetails.classList.remove("hidden");
 
-displaySubmittedInfo();
+    const dLink = document.createElement("a");
+    dLink.innerHTML = "	&#9249;";
+    accountDetails.append(dLink);
+    dLink.id = "delete-link";
+
+    const eLink = document.createElement("a");
+    eLink.innerHTML = "&#9998;";
+    accountDetails.append(eLink);
+    eLink.id = "edit-link";
+  
+    const h3 = document.createElement("h3");
+    h3.innerHTML = account.name;
+    accountDetails.append(h3);
+
+    const h4 = document.createElement("h4");
+    h4.innerHTML = account.nameLast;
+    accountDetails.append(h4);
+
+    const p = document.createElement("p");
+    accountDetails.append(p);
+    p.innerHTML = account.username;
+
+    const h5 = document.createElement("h5");
+    h5.innerHTML = account.password;
+    accountDetails.append(h5);
+
+    const h6 = document.createElement("h6");
+    h6.innerHTML = account.school;
+    accountDetails.append(h6);
+  
+    const ul = document.createElement("ul");
+    accountDetails.append(ul);
+    console.log(account.useages);
+    account.useages.forEach((useage) => {
+      const li = document.createElement("li");
+      ul.append(li);
+      li.innerHTML = useage;
+    });
+  
+    const spoon = document.createElement("section");
+    spoon.classList.add("spoon");
+    accountDetails.append(spoon);
+  
+    eLink.onclick = showAccountForm;
+    dLink.onclick = deleteAccount.bind(this, account);
+  
+    populateEditForm(account);
+  };
+  
+  const populateEditForm = (account)=>{
+    const form = document.getElementById("account-form");
+    form._id.value = account._id;
+    form.name.value = account.name;
+    form.nameLast.value = account.nameLast;
+    form.username.value = account.username;
+    form.password.value = account.password;
+    document.getElementById("img-prev").src = account.img;
+    populateUseages(account.useages);
+  };
+  
+  const populateUseages = (useages)=>{
+    const section = document.getElementById("useage-boxes");
+    useages.forEach((useage)=>{
+      const input = document.createElement("input");
+      input.type = "text";
+      input.value = useage;
+      section.append(input);
+    });
+  };
+  
+  const addEditAccount = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("account-form");
+    const formData = new FormData(form);
+    let response;
+    formData.append("useages", getUseages());
+  
+    console.log(...formData);
+  
+    if (form._id.value.trim() == "") {
+      console.log("in post");
+      response = await fetch("/api/accounts", {
+        method: "POST",
+        body: formData,
+      });
+    } else {
+      console.log("in put");
+      response = await fetch(`/api/accounts/${form._id.value}`,{
+        method:"PUT",
+        body:formData
+      });
+    }
+  
+    if (response.status != 200) {
+      console.log("Error adding / editing data");
+    }
+  
+    await response.json();
+    resetForm();
+    document.getElementById("dialog").style.display = "none";
+    showAccounts();
+  };
+  
+  const getUseages = () => {
+    const inputs = document.querySelectorAll("#useage-boxes input");
+    let useages = [];
+  
+    inputs.forEach((input) => {
+      useages.push(input.value);
+    });
+  
+    return useages;
+  };
+  
+  const resetForm = () => {
+    const form = document.getElementById("account-form");
+    form.reset();
+    form._id.value = "";
+    document.getElementById("useage-boxes").innerHTML = "";
+    document.getElementById("img-prev").src = "";
+  };
+  
+  const showAccountForm = (e) => {
+    openDialog("account-form");
+    console.log(e.target);
+    if (e.target.getAttribute("id") != "edit-link") {
+      resetForm();
+    }
+  };
+  
+  const deleteAccount = async(account) =>{
+    let response = await fetch(`/api/accounts/${account._id}`,{
+      method:"DELETE",
+      headers:{
+        "Content-Type":"application/json;charset=utf-8",
+      },
+    });
+  
+    if(response.status!= 200){
+      console.log("Error deleting");
+      return;
+    }
+  
+    let result = await response.json();
+    resetForm();
+    showAccounts();
+    document.getElementById("dialog").style.display = "none";
+  };
+  
+  const addUseage = (e) => {
+    e.preventDefault();
+    const section = document.getElementById("useage-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    section.append(input);
+  };
+  
+  const openDialog = (id) => {
+    document.getElementById("dialog").style.display = "block";
+    document.querySelectorAll("#dialog-details > *").forEach((item) => {
+      item.classList.add("hidden");
+    });
+    document.getElementById(id).classList.remove("hidden");
+  };
+  
+  showAccounts();
+  document.getElementById("account-form").onsubmit = addEditAccount;
+  document.getElementById("add-link").onclick = showAccountForm;
+  document.getElementById("add-useage").onclick = addUseage;
+  
+  document.getElementById("img").onchange = (e) => {
+    if (!e.target.files.length) {
+      document.getElementById("img-prev").src = "";
+      return;
+    }
+    document.getElementById("img-prev").src = URL.createObjectURL(
+      e.target.files.item(0)
+    );
+  };
--- final/server.js
+++ final/server.js
@@ -1,64 +1,150 @@
 const express = require("express");
 const app = express();
 const Joi = require("joi");
-const mongoose = require("mongoose");
+const multer = require("multer");
 app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://TannerSimpson:0H5jsKGhUoRQFG19@cluster0.emtozuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.log("Error connecting to MongoDB:", error);
-    });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
+
+mongoose
+  .connect(
+    "mongodb+srv://TannerSimpson:0H5jsKGhUoRQFG19@cluster0.emtozuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("couldn't connect to mongodb", error);
+  });
 const accountSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    password: String,
-    school: String,
-    gradeLevel: String
+  name: String,
+  nameLast: String,
+  username: String,
+  password: String,
+  img: String,
+  school: String,
+  useages: [String],
 });
 
 const Account = mongoose.model("Account", accountSchema);
 
-app.get("/accounts", async (req, res) => {
-    try {
-        const accounts = await Account.find();
-        res.json(accounts);
-    } catch (error) {
-        res.status(500).send("Error fetching accounts.");
-    }
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/accounts", async (req, res) => {
-    const schema = Joi.object({
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        username: Joi.string().required(),
-        password: Joi.string().required(),
-        school: Joi.string().required(),
-        gradeLevel: Joi.string().required()
-    });
-
-    try {
-        const validatedData = await schema.validateAsync(req.body);
-
-        const { firstName, lastName, username, password, school, gradeLevel } = validatedData;
-
-        const newAccount = new Account({ firstName, lastName, username, password, school, gradeLevel });
-        await newAccount.save();
-
-        res.status(200).send('Submission successful!');
-    } catch (error) {
-        res.status(400).send(error.details[0].message);
-    }
+app.get("/api/accounts", (req, res) => {
+  getAccounts(res);
 });
+
+const getAccounts = async (res) => {
+  const accounts = await Account.find();
+  res.send(accounts);
+};
+
+app.get("/api/accounts/:id", (req, res) => {
+  getAccount(req.params.id, res);
+});
+
+const getAccount = async (id, res) => {
+  const account = await Account.findOne({ _id: id });
+  res.send(account);
+};
+
+app.post("/api/accounts", upload.single("img"), (req, res) => {
+  const result = validateAccount(req.body);
+  console.log(result);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const account = new Account({
+    name: req.body.name,
+    nameLast: req.body.nameLast,
+    username: req.body.username,
+    password: req.body.password,
+    school: req.body.school,
+    useages: req.body.useages.split(","),
+  });
+
+  if (req.file) {
+    account.img = "images/" + req.file.filename;
+  }
+
+  createAccount(account, res);
+});
+
+const createAccount = async (account, res) => {
+  const result = await account.save();
+  res.send(account);
+};
+
+app.put("/api/accounts/:id", upload.single("img"), (req, res) => {
+  const result = validateAccount(req.body);
+  console.log(result);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  updateAccount(req, res);
+});
+
+const updateAccount = async (req, res) => {
+  let fieldsToUpdate = {
+    name: req.body.name,
+    nameLast: req.body.nameLast,
+    username: req.body.username,
+    password: req.body.password,
+    school: req.body.school,
+    useages: req.body.useages.split(","),
+  };
+
+  if (req.file) {
+    fieldsToUpdate.img = "images/" + req.file.filename;
+  }
+
+  const result = await Account.updateOne({ _id: req.params.id }, fieldsToUpdate);
+
+  res.send(result);
+};
+
+app.delete("/api/accounts/:id", (req, res) => {
+  removeAccount(res, req.params.id);
+});
+
+const removeAccount = async (res, id) => {
+  const account = await Account.findByIdAndDelete(id);
+  res.send(account);
+};
+
+function validateAccount(account) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    nameLast: Joi.string().min(3).required(),
+    username: Joi.string().min(3).required(),
+    password: Joi.string().min(3).required(),
+    school: Joi.string().min(3).required(),
+    useages: Joi.allow(""),
+    _id: Joi.allow(""),
+  });
+
+  return schema.validate(account);
+}
 
 app.listen(3000, () => {
-    console.log("Server is listening ");
+  console.log("I'm listening");
 });
